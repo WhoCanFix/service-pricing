b525224191b132003bda41b1935a966cb9dd3a1e
Kieran Taylor <14927071+kierantaylor@users.noreply.github.com>
2024-06-03 11:00:32 +0100

    fix: component tweak

--- frontend/app/components/heading/heading.component.tsx
+++ frontend/app/components/heading/heading.component.tsx
@@ -1,10 +1,16 @@
-const Heading = () => (
-  <>
-    <div className="leading-[1.2] text-3xl md:text-9xl text-center lg:text-left text-ink-brand-100 font-bold lg:mb-2">
-      Find local garages you can rely on
-    </div>
-    <div className='text-2xl'>We'll help you save money on car repairs in just a few clicks.</div>
-  </>
-);
+import { Component } from "react";
+
+class Heading extends Component {
+  return() {
+    <>
+      <div className="leading-[1.2] text-3xl md:text-9xl text-center lg:text-left text-ink-brand-100 font-bold lg:mb-2">
+        Find local garages you can rely on
+      </div>
+      <div className="text-2xl">
+        We'll help you save money on car repairs in just a few clicks.
+      </div>
+    </>;
+  }
+}
 
 export { Heading };
